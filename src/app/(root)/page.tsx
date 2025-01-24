bf5852b79390a74ed0eca01e4359647e03704d4f
Johnny Silva <jhsilva>
2025-01-24 15:58:35 -0300

    feat: add sample products and product list

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { ProductList } from '@/components/shared/product/product-list'
+import { sampleData } from '@/db/sample-data'
 
 export const metadata = {
   title: 'Home',
 }
 
-export default function Home() {
-  return <Button variant="destructive">Button</Button>
+export default function HomePage() {
+  return <ProductList data={sampleData.products} title="Novidades" limit={2} />
 }
