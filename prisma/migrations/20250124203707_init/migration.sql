1b7490d6562b1faa1a96472c7512a7b45f2a62b4
Johnny Silva <jhsilva>
2025-01-24 17:39:00 -0300

    feat: config neon postgresSQL and add Prisma Models and Migrations

---
+++ prisma/migrations/20250124203707_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "images" TEXT[],
+    "price" DECIMAL(12,2) NOT NULL DEFAULT 0,
+    "rating" DECIMAL(3,2) NOT NULL DEFAULT 0,
+    "num_reviews" INTEGER NOT NULL DEFAULT 0,
+    "brand" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "is_featured" BOOLEAN NOT NULL,
+    "banner" TEXT,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "product_slug_idx" ON "products"("slug");
