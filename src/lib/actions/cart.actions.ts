9731f5121313c2255d91e29ed5002f72412d7244
Johnny Silva <jhsilva>
2025-02-07 15:56:25 -0300

    feat: add price calc  and add to database

--- src/lib/actions/cart.actions.ts
+++ src/lib/actions/cart.actions.ts
@@ -1,13 +1,30 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
 
 import { auth } from '@/auth'
 import { prisma } from '@/db/prisma'
 import { CartItemProps } from '@/types'
 
-import { convertToPlainObject } from '../utils'
-import { cartItemSchema } from '../validators'
+import { convertToPlainObject, round2 } from '../utils'
+import { cartItemSchema, insertCartSchema } from '../validators'
+
+const calPrice = (items: CartItemProps[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, { item }) => acc + Number(item.price) * item.qty, 0),
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 100),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + taxPrice + shippingPrice)
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  }
+}
 
 export async function addItemToCart(data: CartItemProps) {
   try {
@@ -18,7 +35,7 @@ export async function addItemToCart(data: CartItemProps) {
     const session = await auth()
     const userId = session?.user.id ? (session.user.id as string) : undefined
 
-    // const cart = await getMyCart()
+    const cart = await getMyCart()
 
     const item = cartItemSchema.parse(data.item)
 
@@ -28,12 +45,22 @@ export async function addItemToCart(data: CartItemProps) {
       },
     })
 
-    console.log({
-      'Session Cart ID': sessionCartId,
-      'USER ID': userId,
-      'Item Requested': item,
-      'Product Found': product,
-    })
+    if (!product) throw new Error('Product not found')
+
+    if (!cart) {
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calPrice([{ item }]),
+      })
+
+      await prisma.cart.create({
+        data: newCart,
+      })
+
+      revalidatePath(`/product/${product.slug}`)
+    }
 
     return {
       success: true,
@@ -50,22 +77,18 @@ export async function addItemToCart(data: CartItemProps) {
 }
 
 export async function getMyCart() {
-  // Check for cart cookie
   const sessionCartId = (await cookies()).get('sessionCartId')?.value
   if (!sessionCartId) throw new Error('Cart session not found')
 
-  // Get session and user ID
   const session = await auth()
   const userId = session?.user?.id ? (session.user.id as string) : undefined
 
-  // Get user cart from database
   const cart = await prisma.cart.findFirst({
     where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
   })
 
   if (!cart) return undefined
 
-  // Convert decimals and return
   return convertToPlainObject({
     ...cart,
     items: cart.items as CartItemProps[],
